005478f614cbd768d84ccd236d3f71f2baa56d7c
Ez <ez.flight@yandex.ru>
2023-08-26 18:24:10 +0300

    Норм2

--- foodggrambot.py
+++ foodggrambot.py
@@ -2,8 +2,8 @@ import requests
 import os
 import telegram
 from dotenv import load_dotenv
-
-
+from PIL import Image
+ 
 load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
@@ -16,30 +16,26 @@ URL = "http://188.134.84.80/api/recipes/2/"
 # Сделаем GET-запрос к API
 # метод json() преобразует полученный ответ JSON в тип данных, понятный Python
 response = requests.get(URL).json()
-homework_name = response.get("id")
-homework_status = response.get("tags")
 homework_author = response.get("author")
-homework_ingredients = response.get("ingredients")
-homework_is_favorited = response.get("is_favorited")
 food_image = response.get("image")
 homework_cooking_time = response.get("cooking_time")
 # Рассмотрим структуру и содержимое переменной response
 print(food_image)
-
+image = Image.open(str(food_image))
 # Посмотрим, какого типа переменная response
 print(type(food_image))
 
 # response - это список. А какой длины?
 print(len(response))
 #bot.send_photo(TELEGRAM_CHAT_ID, photo=food_image, caption='It works!')
-bot.send_message(TELEGRAM_CHAT_ID, food_image)
+#bot.send_message(TELEGRAM_CHAT_ID, food_image)
+image.show()
+
+#URL = 'https://cdn2.thecatapi.com/images/3dl.jpg'
 
 
-URL = 'https://cdn2.thecatapi.com/images/3dl.jpg'
-
-
-text = 'Вам телеграмма!'
+#text = 'Вам телеграмма!'
 # Отправка сообщения
-bot.send_message(TELEGRAM_CHAT_ID, URL)
+#bot.send_message(TELEGRAM_CHAT_ID, URL)
 # Отправка изображения
-bot.send_photo(TELEGRAM_CHAT_ID, URL) 
+#bot.send_photo(TELEGRAM_CHAT_ID, URL) 
